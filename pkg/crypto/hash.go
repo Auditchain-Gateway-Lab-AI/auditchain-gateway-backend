20e2f9f32f25500d90d57df2102ca3bfa667cb6d
MikoFahreza <mikoyusrizal@gmail.com>
2026-05-19 12:00:22 +0700

    fix dockerfile, pkg/hash, pkg/merkle

--- pkg/crypto/hash.go
+++ pkg/crypto/hash.go
@@ -8,9 +8,6 @@ import (
 
 // GenerateSHA3_256 menghasilkan hash SHA3-256 dari string input
 func GenerateSHA3_256(data string) string {
-	// sha3.Sum256 menghasilkan array byte berukuran 32
 	hash := sha3.Sum256([]byte(data))
-
-	// Konversi byte menjadi string hexadecimal (64 karakter)
 	return hex.EncodeToString(hash[:])
 }
